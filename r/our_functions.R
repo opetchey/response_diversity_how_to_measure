eac2510aaf484e9db182853371b685ca52c249c2
Sam Ross <72502982+SamRPJRoss@users.noreply.github.com>
2022-04-07 11:44:31 +0900

    Added entropy measure
    Added Dave's proposed entropy measure to the supplement (Section 3) to show how it scales with richness, range, overlap with zero etc.

--- r/our_functions.R
+++ r/our_functions.R
@@ -46,7 +46,44 @@ resp_div <- function(x, stat = "range", sign_sens = TRUE) {
   div
   
 }
+
+
+## Function for calculating similarity-based response diversity
+resp_fd <- function(x){
+  d <- dist(x, diag = T) # euclidean distance matrix
+  Z <- exp(as.matrix(-d)) # similarity matrix
+  nspecies <- length(x)
+  p=matrix(1/nspecies,nspecies) # relative abundance matrix. Needs to be changed if evenness is of interest
+  lenq = 1 # initialises hill number. Need to fix if we want any evenness indices
+  qq <- seq(length=lenq, from=0, by=.11)
   
+  # Initialise the Zp matrix to zero
+  Zp=matrix(0,nspecies,1)
+  
+  # Compute Zp
+  for (i in 1:nspecies){
+    for (j in 1:nspecies){
+      Zp[i,1]<-Zp[i,1]+Z[i,j]*p[j,1]
+    }
+  }
+  
+  # Initialise the Diversity matrix to zero
+  Dqz = matrix(0, lenq ,1)
+  
+  for (iq in 1:lenq)  {
+    q<-qq[iq];
+    for (zpi in 1:length(Zp[,1])){
+      if (Zp[zpi,1]>0)(
+        Dqz[iq,1]<-Dqz[iq,1]+ p[zpi,1]*(Zp[zpi,1])^(q-1))
+    }
+    
+    Dqz[iq,1] <- Dqz[iq,1]^(1/(1-q));
+  }
+  Dqz[iq,1]
+}
+
+
+
 # function for standardising range 0-1
 range01 <- function(x) {
   (x-min(x))/(max(x)-min(x))} 
